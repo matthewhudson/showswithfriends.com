f3582e3d5b2329296cfd4f5763786a4533494bd0
Mike Dirolf <mike@dirolf.com>
2012-10-05 16:12:43 -0400

    Config tweaks

--- app/config.py
+++ app/config.py
@@ -8,36 +8,25 @@ class Config(object):
 class ProductionConfig(Config):
     DEBUG = False
     EXCEPTIONAL_API_KEY = 'e34c6ffc7bf25aef4b8528a29f6d36d8da6da556'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "mysql://delete_enabled:godlikepowers@db-ec2-02.seatgeek.com/scrapedb")
-    SEATGEEK_DATABASE_URI = os.environ.get("SG_DATABASE_URL", "mysql://delete_enabled:godlikepowers@db-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SG_DATABASE_URL", "mysql://delete_enabled:godlikepowers@db-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay")
     PORT = 9600
     SG_CLIENT_KEY = '' # REPLACE
     SG_CLIENT_SEC = '' # REPLACE
-    SQLALCHEMY_BINDS = {
-        'seatgeek': SEATGEEK_DATABASE_URI,
-    }
 
 class StagingConfig(Config):
     DEBUG = False
     EXCEPTIONAL_API_KEY = ''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "mysql://root@localhost:3306/scrapedb")
-    SEATGEEK_DATABASE_URI = os.environ.get("SG_DATABASE_URL", "mysql://delete_enabled:godlikepowers@backup-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SG_DATABASE_URL", "mysql://delete_enabled:godlikepowers@backup-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay")
     PORT = 9600
     SG_CLIENT_KEY = '' # REPLACE
     SG_CLIENT_SEC = '' # REPLACE
-    SQLALCHEMY_BINDS = {
-        'seatgeek': SEATGEEK_DATABASE_URI,
-    }
 
 class DevelopmentConfig(Config):
     '''Use "if app.debug" anywhere in your code, that code will run in development code.'''
     DEBUG = True
+    PRESERVE_CONTEXT_ON_EXCEPTION = True
     EXCEPTIONAL_API_KEY = ''
     # EXCEPTIONAL_DEBUG_URL = 'http://requestb.in/1kmlz0e1'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:8889/scrapedb4?unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock'
-    SEATGEEK_DATABASE_URI = 'mysql://delete_enabled:godlikepowers@backup-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay'
+    SQLALCHEMY_DATABASE_URI = 'mysql://delete_enabled:godlikepowers@backup-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay'
     SG_CLIENT_KEY = '' # REPLACE
     SG_CLIENT_SEC = '' # REPLACE
-    SQLALCHEMY_BINDS = {
-        'seatgeek': SEATGEEK_DATABASE_URI,
-    }
