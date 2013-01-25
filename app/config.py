af9c765c1a06995646bd1569262c2cde43021ad0
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2013-01-25 16:27:55 -0500

    PEP8

--- app/config.py
+++ app/config.py
@@ -1,23 +1,27 @@
 import os
 
+
 class Config(object):
     SECRET_KEY = 'REPLACE: SECRET KEY'
     SITE_NAME = 'REPLACE: PROJECT NAME'
     PORT = int(os.getenv('PORT', '5000'))
 
+
 class ProductionConfig(Config):
     DEBUG = False
     EXCEPTIONAL_API_KEY = 'e34c6ffc7bf25aef4b8528a29f6d36d8da6da556'
     SQLALCHEMY_DATABASE_URI = os.environ.get("SG_DATABASE_URL", "mysql://delete_enabled:godlikepowers@db-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay")
-    SG_CLIENT_KEY = '' # REPLACE
-    SG_CLIENT_SEC = '' # REPLACE
+    SG_CLIENT_KEY = ''  # REPLACE
+    SG_CLIENT_SEC = ''  # REPLACE
+
 
 class StagingConfig(Config):
     DEBUG = False
     EXCEPTIONAL_API_KEY = ''
     SQLALCHEMY_DATABASE_URI = os.environ.get("SG_DATABASE_URL", "mysql://delete_enabled:godlikepowers@backup-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay")
-    SG_CLIENT_KEY = '' # REPLACE
-    SG_CLIENT_SEC = '' # REPLACE
+    SG_CLIENT_KEY = ''  # REPLACE
+    SG_CLIENT_SEC = ''  # REPLACE
+
 
 class DevelopmentConfig(Config):
     '''Use "if app.debug" anywhere in your code, that code will run in development code.'''
@@ -26,5 +30,5 @@ class DevelopmentConfig(Config):
     EXCEPTIONAL_API_KEY = ''
     # EXCEPTIONAL_DEBUG_URL = 'http://requestb.in/1kmlz0e1'
     SQLALCHEMY_DATABASE_URI = 'mysql://delete_enabled:godlikepowers@backup-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay'
-    SG_CLIENT_KEY = '' # REPLACE
-    SG_CLIENT_SEC = '' # REPLACE
+    SG_CLIENT_KEY = ''  # REPLACE
+    SG_CLIENT_SEC = ''  # REPLACE
