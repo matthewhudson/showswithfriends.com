51ba44cabeee19545f917ff8df926a969ef97af4
Steve Ritter <steve@seatgeek.com>
2013-02-21 11:40:11 -0500

    default to prod

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ flask_app = Flask(__name__)
 import app.views
 
 # Configuration
-if os.getenv('SG_ENV', "dev") == 'dev':
+if os.getenv('SG_ENV', "prod") == 'dev':
     flask_app.config.from_object('app.config.DevelopmentConfig')
     flask_app.logger.info("Config: Development")
 elif os.getenv('SG_ENV') == 'staging':
