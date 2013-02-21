151b867ea2ebfa49a996041267de7b69d320accd
Steve Ritter <steve@seatgeek.com>
2013-02-21 10:42:45 -0500

    db uris

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Config(object):
 class ProductionConfig(Config):
     DEBUG = False
     EXCEPTIONAL_API_KEY = 'e34c6ffc7bf25aef4b8528a29f6d36d8da6da556'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SG_DATABASE_URL", "mysql://delete_enabled:godlikepowers@db-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", 'postgres://mlfaerdqmihwqt:5uioakbw8XgNzAR1pNA6NE4RAS@ec2-107-21-122-236.compute-1.amazonaws.com:5432/dfmmm3r5jodsua')
     SG_CLIENT_KEY = 'MjA3OTM1fDEzNjE0NTkyNTM'
     SG_CLIENT_SEC = 'WWqj2_4lUDZ903aMXtd1IXFbu1RoivejRqI5B7wi'
 
@@ -18,7 +18,7 @@ class ProductionConfig(Config):
 class StagingConfig(Config):
     DEBUG = False
     EXCEPTIONAL_API_KEY = ''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SG_DATABASE_URL", "mysql://delete_enabled:godlikepowers@backup-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", 'postgres://mlfaerdqmihwqt:5uioakbw8XgNzAR1pNA6NE4RAS@ec2-107-21-122-236.compute-1.amazonaws.com:5432/dfmmm3r5jodsua')
     SG_CLIENT_KEY = 'MjA3OTM1fDEzNjE0NTkyNTM'
     SG_CLIENT_SEC = 'WWqj2_4lUDZ903aMXtd1IXFbu1RoivejRqI5B7wi'
 
@@ -29,6 +29,6 @@ class DevelopmentConfig(Config):
     PRESERVE_CONTEXT_ON_EXCEPTION = True
     EXCEPTIONAL_API_KEY = ''
     # EXCEPTIONAL_DEBUG_URL = 'http://requestb.in/1kmlz0e1'
-    SQLALCHEMY_DATABASE_URI = 'mysql://delete_enabled:godlikepowers@backup-ec2-02.seatgeek.com/tzanalytic_tixcast_ebay'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/showtunes'
     SG_CLIENT_KEY = 'MjA3OTM1fDEzNjE0NTkzOTg'
     SG_CLIENT_SEC = '5tu1x0pYx8R_ouab_ZAeVJTUP-lzTelMJMP3qePi'
