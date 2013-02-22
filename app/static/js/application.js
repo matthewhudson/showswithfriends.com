fecafba81c052d9ac94e7fd0024dbcda4165c1bc
Matthew Hudson <matt@matthewghudson.com>
2013-02-22 01:37:18 -0500

    Integrated core frontend code.

---
+++ app/static/js/application.js
@@ -0,0 +1,140 @@
+$(function () {
+
+  var body_height = 0, first_event;
+
+  var $intro = $('#overview .container .intro');
+
+  _.templateSettings.variable = "rc";
+
+  var event_template = _.template($("script.event-template").html());
+
+  var hero_template = _.template($("script.hero-template").html());
+
+  var modal = function () {
+    var my = {}, that = {};
+    my.el = $('#overlay');
+    
+    my.template = _.template($("script.modal-template").html());
+
+    my.el.click(function () {
+      that.hide();
+    });
+
+    that.show = function () {
+      $('body').css('overflow', 'hidden');
+      my.el.show();
+    };
+
+    that.hide = function () {
+      $('body').css('overflow', 'auto');
+      my.el.hide();
+    };
+
+    that.addContent = function(data) {
+      $('.artist-bg').css('background', "url('" + data.image + "')");
+      my.el.html(my.template({
+        title : data.title,
+        url : data.url,
+        venue_name : data.venue_name,
+        venue_city : data.venue_city,
+        image : data.image,
+        datetime : data.datetime
+      }));
+    };
+
+    return that;
+  };
+
+
+  var $content = $(".content .row");
+  seatgeek.events({ sort: 'score.desc' , 'taxonomies.id': 2000000 }, function (d) {
+    _.each(d.events, function (event) {
+      console.log(event);
+      if (!first_event) first_event = event.id.toString();
+      $content.append(event_template({
+        id: event.id,
+        title: event.title,
+        url: event.url,
+        venue: event.venue,
+        image: event.performers[0].image || '/assets/img/head-center-experiment.jpg',
+        datetime: Date.parse(event.datetime_utc).toString("dddd, MMMM d, yyyy @ h:mm:ss tt")
+      }));
+    });
+  });
+
+  var $hero = $('#overview .container .info');
+  $('.event').livequery(function () {
+    $(this).waypoint(function (direction) {
+      if (direction === 'down') {
+        $intro.hide();
+        $hero.show();
+        $(this).stop().animate({ opacity: 0 }, { queue : false }, 'fast');
+      } else {
+        $(this).stop().animate({ opacity: 1 }, { queue : false }, 'fast');
+
+        // Reset the hero unit if scrolling to top from bottom
+        if ($(this).find('.id').html() === first_event) {
+          $hero.hide();
+          $intro.show();
+        }
+      }
+      $hero.html(hero_template({
+        title : ellipsis($(this).find('.title b').text()),
+        url : $(this).find('.url').html(),
+        venue_name : $(this).find('.venue-name').html(),
+        venue_city : $(this).find('.venue-city').html(),
+        image : $(this).find('.image').attr('src'),
+        datetime : $(this).find('.datetime').html()
+      }));
+
+      // HACK
+      body_height = $('body').height()
+    }, {
+      offset: 280
+    });
+  });
+  
+  $('.line').livequery(function () {
+    $(this).css('height', body_height);
+  });
+
+  $('.box .image').livequery(function () {
+    $(this).waypoint(function (direction) {
+      if (direction === 'down') {
+        $(this).stop().fadeOut(600).animate({ width: 250, height: 250, borderRadius: 125, left: -200 }, { queue : false }, 'slow');
+      } else {
+        $(this).stop().fadeIn(600).animate({ width: 120, height: 120, borderRadius: 60, left: -136 }, { queue : false }, 'slow');
+      }
+    }, {
+      offset: 300
+    });
+  });
+
+  function ellipsis (string) {
+    if (string.length > 45) {
+      return string = string.substring(0, 44) + "...";
+    }
+    return string
+  }
+  modal = modal();
+  
+  var data = null;
+  $('.info .btn').livequery(function () {
+    $(this).click(function (ev) {
+
+      ev.preventDefault();
+      data = $(this).parent().parent();
+
+      modal.addContent({
+        title : data.find('.title').text(),
+        url : data.find('.url').html(),
+        venue_name : data.find('.venue-name').html(),
+        venue_city : data.find('.venue-city').html(),
+        image : data.find('.image').attr('src'),
+        datetime : data.find('.datetime').html()
+      })
+      modal.show();
+      return false;
+    });
+  });
+});
