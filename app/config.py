29a83abdc6f1a061481bac533a0cd6f8cce61829
Steve Ritter <steve@seatgeek.com>
2013-02-22 00:03:01 -0500

    rmv exceptional

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Config(object):
 class ProductionConfig(Config):
     DEBUG = True
     PRESERVE_CONTEXT_ON_EXCEPTION = True
-    EXCEPTIONAL_API_KEY = 'e34c6ffc7bf25aef4b8528a29f6d36d8da6da556'
+    EXCEPTIONAL_API_KEY = '' #'e34c6ffc7bf25aef4b8528a29f6d36d8da6da556'
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", 'postgres://mlfaerdqmihwqt:5uioakbw8XgNzAR1pNA6NE4RAS@ec2-107-21-122-236.compute-1.amazonaws.com:5432/dfmmm3r5jodsua')
     SG_CLIENT_KEY = 'MjA3OTM1fDEzNjE0NTkyNTM'
     SG_CLIENT_SEC = 'WWqj2_4lUDZ903aMXtd1IXFbu1RoivejRqI5B7wi'
